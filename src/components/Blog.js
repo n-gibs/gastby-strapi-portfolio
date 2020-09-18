a5fa0caffb68b2048fc4294a8d14788e037d0d5a
nikkig124 <nikkig124@gmail.com>
2020-09-18 11:54:41 -0500

    fix blog stuff

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,20 +2,24 @@ import React from "react"
 import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { Link } from "gatsby"
-const Blog = ({id, title, image, date, category, slug, description}) => {
-  return <Link to={`blogs/${slug}`} key={id} className="blog">
-    <article>
-      <Image fluid={image.childImageSharp.fluid} className="blog-img" />
-      <div className="blog-card">
-        <h4>{title}</h4>
-        <p>{description}</p>
-        <div className="blog-footer">
-          <p>{category}</p>
-          <p>{date}</p>
+const Blog = ({ id, title, image, date, category, slug, desc }) => {
+  return (
+    <Link to={`/blogs/${slug}`} className="blog" key={id}>
+      <article>
+        {image && (
+          <Image fluid={image.childImageSharp.fluid} className="blog-img" />
+        )}
+        <div className="blog-card">
+          <h4>{title}</h4>
+          <p>{desc}</p>
+          <div className="blog-footer">
+            <p>{category}</p>
+            <p>{date}</p>
+          </div>
         </div>
-      </div>
-    </article>
-  </Link>
+      </article>
+    </Link>
+  )
 }
 
 Blog.propTypes = {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,25 +2,33 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
+import SEO from "../components/SEO"
 
-const Blog = ({data:{allStrapiBlogs:{nodes:blogs}}}) => {
-  return <Layout>
-    <section className="blog-page">
-      <Blogs blogs={blogs} title="blogs"/>
-    </section>
-  </Layout>
+const Blog = ({
+  data: {
+    allStrapiBlogs: { nodes: blogs },
+  },
+}) => {
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <section className="blog-page">
+        <Blogs blogs={blogs} title="blog" />
+      </section>
+    </Layout>
+  )
 }
 
 export const query = graphql`
   {
-    allStrapiBlogs{
+    allStrapiBlogs {
       nodes {
-        id
         slug
+        desc:description
+        date(formatString: "MMMM Do, YYYY")
+        id
         title
         category
-        description
-        date(formatString: "MMMM, DD, YYY")
         image {
           childImageSharp {
             fluid {
@@ -32,5 +40,4 @@ export const query = graphql`
     }
   }
 `
-
 export default Blog
